05ce13c7e97987c507a169992ae405a832ae9822
AnalystYuchels <uche4xploit@gmail.com>
2026-03-25 19:11:31 +0100

    add ed express.static for public folder and enabled URL-encoded body parsing for form submission

--- Secrets Project/index.js
+++ Secrets Project/index.js
@@ -1,9 +1,15 @@
 // HINTS:
 // 1. Import express and axios
+import express from "express";
+import axios from "axios";
 
 // 2. Create an express app and set the port number.
+const app = express();
+const port = 3000;
 
 // 3. Use the public folder for static files.
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 // 4. When the user goes to the home page it should render the index.ejs file.
 
