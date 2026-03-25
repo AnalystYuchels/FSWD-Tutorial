ee6c8e1e05747a4496923c4d09f962a2bc0171d8
AnalystYuchels <uche4xploit@gmail.com>
2026-03-25 16:59:05 +0100

    implement API key authentication with query parameters for filtered secrets

--- API Authentication/index.js
+++ API Authentication/index.js
@@ -58,10 +58,24 @@ app.get("/basicAuth", async (req, res) => {
   }
 });
 
-app.get("/apiKey", (req, res) => {
+app.get("/apiKey", async (req, res) => {
   //TODO 4: Write your code here to hit up the /filter endpoint
   //Filter for all secrets with an embarassment score of 5 or greater
   //HINT: You need to provide a query parameter of apiKey in the request.
+  try {
+    const response = await axios.get(API_URL + "filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+
+    res.render("index.ejs", {
+      content: JSON.stringify(response.data),
+    });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.get("/bearerToken", (req, res) => {
