7428a9bfb4d9929881818ddbc908325d1602304d
AnalystYuchels <uche4xploit@gmail.com>
2026-04-07 16:55:45 +0100

    Fix JSON body parsing by editing express.json middleware

--- Blog API Project/index.js
+++ Blog API Project/index.js
@@ -35,7 +35,7 @@ let posts = [
 let lastId = 3;
 
 // Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Write your code here//
